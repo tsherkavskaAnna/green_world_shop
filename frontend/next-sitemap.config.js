9433765d747e9b607cb2345096b3ecd07152e066
Ania <80473872+tsherkavskaAnna@users.noreply.github.com>
2025-03-14 13:01:52 +0100

    change sitemap file

--- frontend/next-sitemap.config.js
+++
@@ -1,3 +0,0 @@
-export const siteUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
-export const generateRobotsTxt = true;
-export const sitemapSize = 7000;
---
+++ frontend/next-sitemap.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  // eslint-disable-next-line no-undef
+  siteUrl: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
