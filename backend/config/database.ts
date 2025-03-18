22f6dad8a52af1d46b8540e2f0b91e4ed3dc48a4
Ania <80473872+tsherkavskaAnna@users.noreply.github.com>
2025-03-18 22:06:57 +0100

    add connection string with db

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -2,12 +2,14 @@ module.exports = ({ env }) => ({
 	connection: {
 		client: 'postgres',
 		connection: {
-			host: env('DATABASE_HOST', 'localhost'),
-			port: env.int('DATABASE_PORT', 5432),
-			database: env('DATABASE_NAME', 'cms_green_world'),
-			user: env('DATABASE_USERNAME', 'main_user'),
-			password: env('DATABASE_PASSWORD', '12071957'),
-			ssl: env.bool('DATABASE_SSL', false),
+			host: env('DATABASE_HOST'),
+			port: env.int('DATABASE_PORT'),
+			database: env('DATABASE_NAME'),
+			user: env('DATABASE_USERNAME'),
+			password: env('DATABASE_PASSWORD'),
+			ssl: { rejectUnauthorized: false },
 		},
+		pool: { min: 0, max: 10, acquireTimeoutMillis: 30000 },
+		debug: false,
 	},
 });
